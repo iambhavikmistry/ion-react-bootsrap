909d74882838c98118fe07dbc85e6203a622d116
Bhavik Mistry <iambhavikmistry@gmail.com>
2024-04-07 21:24:45 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ion.bootstrap',
+  appName: 'ion-bootstrap',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
